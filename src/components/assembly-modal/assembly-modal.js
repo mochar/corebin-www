070fdbaac5b93f9da1fe3e12ccfb3e30260a4c7e
Mo Char <mocharrout@gmail.com>
2016-05-17 01:47:03 +0200

    Added coverage input to assembly modal

--- src/components/assembly-modal/assembly-modal.js
+++ src/components/assembly-modal/assembly-modal.js
@@ -5,9 +5,47 @@ define(['knockout', 'text!./assembly-modal.html', 'knockout-postbox'], function(
         
         self.assemblies = ko.observableArray([]).syncWith('assemblies');
         
+        self.resetForm = function() {
+            self.hasHeaders = ko.observable(true);
+            self.samples = ko.observableArray([]);
+            self._samples = [];
+        };
+        self.resetForm();
+        
+        self.onFileChange = function(file) {
+            var reader = new FileReader();
+            reader.onload = function() {
+                var allTextLines = this.result.split(/\r\n|\n/);
+                var headers = allTextLines[0].replace(/\t/g, ',').split(',');
+                self._samples = headers.slice(1);
+                self.samples(self._samples);
+            };
+            reader.readAsText(file);
+        };
+        
+        ko.computed(function() {
+            var hasHeaders = self.hasHeaders();
+            var samples = [];
+            for (var i = 0; i < self._samples.length; i++) {
+                var j = i + 1;
+                samples.push('sample_' + j);
+            }
+            self.samples(hasHeaders ? self._samples : samples);
+        });
+        
         self.uploadAssembly = function(formElement) {
             var formData = new FormData(formElement);
+            
+            // Add sample names
+            var samples = self.samples()
+            for (var i = 0; i < samples.length; i++) {
+                formData.append('samples[]', samples[i]);
+            }
+            
+            // Reset form
             formElement.reset();
+            self.resetForm();
+            
             $.ajax({
                 url: '/a',
                 type: 'POST',
