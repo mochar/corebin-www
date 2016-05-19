60b9834a5ece39988450f8791dbf626261e31e56
Mo Char <mocharrout@gmail.com>
2016-05-19 10:29:37 +0200

    Added frequency plot for contig lengths

--- src/components/assembly-page/assembly-page.js
+++ src/components/assembly-page/assembly-page.js
@@ -24,7 +24,32 @@ define([
                 var lengths = hist.map(function(bin) { return bin.length; });
                 self.gcData([['x'].concat(x.slice(0, -1)), 
                              ['frequency'].concat(lengths)]);
-            })
+            });
+            
+            $.getJSON(url, {fields: 'length', items: 10000}, function(data) {
+                var x = ['< 1.0 kb', '1.0 - 3.5 kb', '3.5 - 7.0 kb', '7.0 - 15.0 kb', 
+                         '15.0 - 30.0 kb', '30.0 - 60.0 kb', '> 60.kb'];
+                var frequency = [0, 0, 0, 0, 0, 0, 0];
+                data.contigs.forEach(function(c) { 
+                    if (c.length < 1000) {
+                        frequency[0]++;
+                    } else if (c.length < 3500) {
+                        frequency[1]++;
+                    } else if (c.length < 7000) {
+                        frequency[2]++;
+                    } else if (c.length < 15000) {
+                        frequency[3]++;
+                    } else if (c.length < 30000) {
+                        frequency[4]++;
+                    } else if (c.length < 60000) {
+                        frequency[5]++;
+                    } else {
+                        frequency[6]++;
+                    }
+                });
+                self.lengthData([['x'].concat(x), 
+                                 ['frequency'].concat(frequency)]);
+            });
         }, true);
     };
     
