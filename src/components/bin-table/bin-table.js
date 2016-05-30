1c50271cc93da45bcc04c2b672a8d71dbc7b392f
Mo Char <mocharrout@gmail.com>
2016-05-30 16:07:58 +0200

    Moved bin table to own component

---
+++ src/components/bin-table/bin-table.js
@@ -0,0 +1,36 @@
+define(['knockout', 'text!./bin-table.html', 'knockout-postbox'], function(ko, template) {
+
+    function ViewModel(params) {
+        var self = this;
+        
+        self.bins = params.bins;
+        self.binSet = params.binSet;
+        self.selectedBins = params.selectedBins;
+        self.loading = params.loading;
+        self.newBin = ko.observable(false);
+        
+        self.createBin = function(formElement) {
+            var binSet = self.binSet();
+            if (!binSet) return;
+            var formData = new FormData(formElement);
+            $.ajax({
+                url: '/a/' + binSet.assembly + '/bs/' + binSet.id + '/b',
+                type: 'POST',
+                data: formData,
+                async: false,
+                success: function(data) {
+                    self.bins.push(data);
+                    formElement.reset();
+                },
+                cache: false,
+                contentType: false,
+                processData: false
+            });
+        }
+    };
+    
+    return {
+        viewModel: ViewModel,
+        template: template
+    };
+});
