d62db9641e462e35955b513ce05874f9344b028c
Mo Char <mocharrout@gmail.com>
2016-07-06 19:39:09 +0200

    Assembly table load contigs of bins instead of assembly

--- src/components/assembly-table/assembly-table.js
+++ src/components/assembly-table/assembly-table.js
@@ -5,11 +5,14 @@ define(['knockout', 'text!./assembly-table.html', 'knockout-postbox'], function(
         
         self.contigs = ko.observableArray([]);
         self.loading = ko.observable(true);
+        self.assembly = ko.observable().subscribeTo('assembly', true);
         
-        ko.postbox.subscribe('assembly', function(assembly) {
+        ko.postbox.subscribe('bin', function(bin) {
+            self.contigs([]);
             self.loading(true);
-            if (!assembly) return;
-            $.getJSON('/a/' + assembly.id + '/c', {'items': 10}, function(data) {
+            if (!bin) return;
+            var data = {items: 15, bins: bin.id};
+            $.getJSON('/a/' + self.assembly().id + '/c', data, function(data) {
                 self.contigs(data.contigs);
                 self.loading(false);
             })
