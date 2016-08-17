05e62f36499331ff7a343e67dce49fd79178208a
Lex Overmars <L.Overmars@gmail.com>
2016-08-17 09:36:29 +0200

    fix

--- src/components/compare-page/compare-page.js
+++ src/components/compare-page/compare-page.js
@@ -27,7 +27,7 @@ define(['knockout', 'text!./compare-page.html', 'jquery', 'knockout-postbox'], f
                 otherBinSet = self.otherBinSet(),
                 by = self.by(),
                 params = {binset1: binSet.id, binset2: otherBinSet.id, by: by};
-            if (otherBinSet.id == null) {
+            if (otherBinSet == null) {
      			alert("Please select a bin-set to compare with");
      			return;
 			}
